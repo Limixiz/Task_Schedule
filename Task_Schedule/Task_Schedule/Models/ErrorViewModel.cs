a18229f9c46a90b2886f9214893764d78a0f122e
Limixiz <61080263+Limixiz@users.noreply.github.com>
2021-02-17 21:51:24 +0100

    1

---
+++ Task_Schedule/Task_Schedule/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Task_Schedule.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Task_Schedule/Task_Schedule/Models/TaskModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_Schedule.Models
+{
+    public class TaskModel
+    {
+        public string TaskName { get; set; }
+        public DateTime TaskStart { get; set; }
+        public bool TaskAllDay { get; set; }
+        public DateTime TaskEnd { get; set; }
+        public string TaskDescription { get; set; }
+    }
+}
